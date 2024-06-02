a87a7f3f3783285016e64900af673a967bf9c663
ben4ali <alibenkarrouch@gmail.com>
2024-06-02 11:23:21 -0400

    Final final push

--- src/ExempleCoursException/ExempleCours01/src/ExempleCoursException/ExceptionBof.java
+++
@@ -1,9 +0,0 @@
-package ExempleCoursException;
-
-public class ExceptionBof extends Exception{
-    public ExceptionBof(String message)
-    {
-        super("Bof ! " + message);
-
-    }
-}
--- src/main/java/org/example/ExempleCoursException/ExempleCours01/src/ExempleCoursException/ExceptionMenfin.java
+++
@@ -1,9 +0,0 @@
-package ExempleCoursException;
-
-public class ExceptionMenfin extends Exception{
-    public ExceptionMenfin(String message)
-    {
-        super("M'enfin ! " + message);
-
-    }
-}
